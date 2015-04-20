f0c8dce47fc30251d6754529b2fa5a47d1f3dc3a
AndreyNick <askneket@gmail.com>
2015-04-20 11:02:52 +0300

    Added log4j properties file

---
+++ log/src/main/java/com/gym/Log.java
@@ -0,0 +1,22 @@
+package com.gym;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.util.Arrays;
+
+/**
+ * Created by anni0913 on 16.04.2015.
+ */
+@Aspect
+public class Log {
+
+    public static Logger log = Logger.getLogger(Log.class);
+
+    @AfterReturning(pointcut = "execution(* com.gym.service.AbstractGenericService.*(..))")
+    public void abstractGenericServiceLog(JoinPoint joinPoint) throws Throwable {
+        log.info(joinPoint.getSignature() + " " + Arrays.toString(joinPoint.getArgs()));
+    }
+}
