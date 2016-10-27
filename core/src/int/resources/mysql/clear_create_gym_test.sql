28b7ebb893dc515252ea6568ea877bd9077fd0fb
overriderb <overriderb@gmail.com>
2016-10-27 22:06:34 +0300

    Added condition "if not exists" to sql script, for all table creation

--- core/src/int/resources/mysql/clear_create_gym_test.sql
+++ core/src/int/resources/mysql/clear_create_gym_test.sql
@@ -5,7 +5,7 @@ DROP DATABASE `gym_test`$$
 CREATE DATABASE `gym_test`$$
 USE `gym_test`$$
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `user_id` INT (11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR (1000) NOT NULL,
   `login` VARCHAR (1000) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `user` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
-CREATE TABLE `persistent_logins` (
+CREATE TABLE IF NOT EXISTS `persistent_logins` (
   `username` VARCHAR(64) NOT NULL,
   `series` VARCHAR(64) NOT NULL,
   `token` VARCHAR(64) NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE `persistent_logins` (
   PRIMARY KEY (`series`)
 ) DEFAULT CHARSET=utf8$$;
 
-CREATE TABLE `role` (
+CREATE TABLE IF NOT EXISTS `role` (
   `role_id` INT (11) NOT NULL AUTO_INCREMENT,
   `user_id` INT (11) NOT NULL,
   `role` VARCHAR(45) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE `role` (
   CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
-CREATE TABLE `program_template` (
+CREATE TABLE IF NOT EXISTS `program_template` (
   `program_template_id` INT (11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR (1000) NOT NULL,
   `description` VARCHAR (10000),
@@ -39,7 +39,7 @@ CREATE TABLE `program_template` (
   PRIMARY KEY (`program_template_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
-CREATE TABLE `exercise_template` (
+CREATE TABLE IF NOT EXISTS `exercise_template` (
   `exercise_template_id` INT (11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR (1000) NOT NULL,
   `description` VARCHAR (10000),
@@ -47,7 +47,7 @@ CREATE TABLE `exercise_template` (
   PRIMARY KEY (`exercise_template_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
-CREATE TABLE `program_exercise_templates` (
+CREATE TABLE IF NOT EXISTS `program_exercise_templates` (
   `program_exercise_template_id` INT (11) NOT NULL AUTO_INCREMENT,
   `program_template_id` INT (11) NOT NULL,
   `exercise_template_id` INT (11) NOT NULL,
@@ -58,7 +58,7 @@ CREATE TABLE `program_exercise_templates` (
   CONSTRAINT `fk_exercise_template` FOREIGN KEY (`exercise_template_id`) REFERENCES `exercise_template` (`exercise_template_id`)
 ) DEFAULT CHARSET=utf8$$
 
-CREATE TABLE `exercise` (
+CREATE TABLE IF NOT EXISTS `exercise` (
   `exercise_id` INT (11) NOT NULL AUTO_INCREMENT,
   `program_id` INT (11) NOT NULL,
   `exercise_template_id` INT (11) NOT NULL,
@@ -68,7 +68,7 @@ CREATE TABLE `exercise` (
   PRIMARY KEY (`exercise_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
-CREATE TABLE `program` (
+CREATE TABLE IF NOT EXISTS `program` (
   `program_id` INT (11) NOT NULL AUTO_INCREMENT,
   `user_id` INT (11) NOT NULL,
   `name` VARCHAR (1000) NOT NULL,
@@ -78,7 +78,7 @@ CREATE TABLE `program` (
   PRIMARY KEY (`program_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
-CREATE TABLE `approach` (
+CREATE TABLE IF NOT EXISTS `approach` (
   `approach_id` INT (11) NOT NULL AUTO_INCREMENT,
   `exercise_id` INT (11) NOT NULL,
   `times` INT (5),
