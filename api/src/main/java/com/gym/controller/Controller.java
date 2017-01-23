8d27a09216a12bf9ccc43de75d36d6aa7945bbe8
AndreyNick <askneket@gmail.com>
2017-01-23 15:58:53 +0200

    -api module was added

--- api/src/main/java/com/gym/controller/Controller.java
+++ api/src/main/java/com/gym/controller/Controller.java
@@ -1,24 +1,34 @@
 package com.gym.controller;
 
+import com.gym.objects.User;
+import com.gym.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 public class Controller {
 
-    //@Autowired
-    //public UserService userService;
+    @Autowired
+    public UserService userService;
 
-    /*@RequestMapping(value="/users",
-            method = RequestMethod.GET)
+    @RequestMapping(value="/users",
+            method = RequestMethod.GET,
+            headers="Accept=application/json")
+    @ResponseBody
     public List<User> getUsers() {
         return userService.readAll();
-    }*/
+    }
 
     @RequestMapping(value="/test",
-            method = RequestMethod.GET)
+            method = RequestMethod.GET,
+            headers="Accept=application/json")
+    @ResponseBody
     public String test() {
         return "test";
     }
