8f55d5459991ede0d9816a8af47b6ba6bd601358
AndreyNick <askneket@gmail.com>
2017-02-23 14:35:47 +0200

    -some minor changes

--- api/src/main/java/com/gym/controller/Controller.java
+++ api/src/main/java/com/gym/controller/Controller.java
@@ -3,13 +3,12 @@ package com.gym.controller;
 import com.gym.objects.User;
 import com.gym.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 
 @RestController
 public class Controller {
@@ -17,21 +16,21 @@ public class Controller {
     @Autowired
     public UserService userService;
 
+    @RequestMapping(value="/",
+            method = RequestMethod.GET)
+    public String test() {
+        return "it works";
+    }
+
     @RequestMapping(value="/users",
-            method = RequestMethod.GET,
-            headers="Accept=application/json")
-    @ResponseBody
+            method = RequestMethod.GET)
     public List<User> getUsers() {
         return userService.readAll();
     }
 
-    @RequestMapping(value="/test",
-            method = RequestMethod.GET,
-            headers="Accept=application/json")
-    @ResponseBody
-    public String test() {
-        return "test";
+    @RequestMapping(value="/user/{id}",
+            method = RequestMethod.GET)
+    public User getUser(@PathVariable("id") int id) {
+        return userService.read((long) id);
     }
-
-
 }
