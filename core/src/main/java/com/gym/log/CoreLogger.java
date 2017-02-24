fb8008daed092d04566a40f9e0c87c677d802efb
AndreyNick <askneket@gmail.com>
2017-02-24 17:15:59 +0200

    -log was moved to core

--- core/src/main/java/com/gym/log/CoreLogger.java
+++ core/src/main/java/com/gym/log/CoreLogger.java
@@ -1,4 +1,5 @@
-package com.gym;
+package com.gym.log;
+
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
@@ -8,9 +9,9 @@ import org.aspectj.lang.annotation.Aspect;
 import java.util.Arrays;
 
 @Aspect
-public class Log {
+public class CoreLogger {
 
-    public static Logger log = Logger.getLogger(Log.class);
+    public static Logger log = Logger.getLogger(CoreLogger.class);
 
     @AfterReturning(pointcut = "execution(* com.gym.service.AbstractGenericService.*(..))")
     public void abstractGenericServiceLog(JoinPoint joinPoint) throws Throwable {
